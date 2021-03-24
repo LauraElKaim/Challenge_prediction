7b6a9c1c86d2705bfd7e249f1b4aef3498c6c66c
unknown <laura.el-kaim@domaine.fr>
2021-03-24 23:28:36 +0100

    Deletion of the 'Heure / time' column

--- bikeprediction/preprocess/format_date.py
+++ bikeprediction/preprocess/format_date.py
@@ -3,4 +3,5 @@ import pandas as pd
 def date_formated(self):
     df = pd.to_datetime(self['Date'] + ' ' + self['Heure / Time'], format = '%d/%m/%Y %H:%M:%S')
     self['Date'] = df
+    del self['Heure / Time']
     return self
