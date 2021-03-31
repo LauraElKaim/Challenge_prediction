01c34afa9304edaffb99292c02a7d2c5b82d7182
unknown <laura.el-kaim@domaine.fr>
2021-03-31 10:50:39 +0200

    Creation of the bikeprediction package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from biketrauma import __version__ as current_version
+
+setup(
+  name='bikeprediction_elk',
+  version=current_version,
+  description='Prediction and visualization of bike traffic in Montpellier',
+  url='https://github.com/LauraElKaim/Challenge_prediction.git',
+  author='ElKaimLaura',
+  author_email='laura.el-kaim@etu.umontpellier.fr',
+  license='MIT',
+  packages=['bikeprediction','bikeprediction.data', 'bikeprediction.io',  'bikeprediction.preprocess', 'bikeprediction.vis'],
+  zip_safe=False
+)
